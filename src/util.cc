07e5a453f4d49496d08f499a53908d17be9a72d3
KireinaHoro <i@jsteward.moe>
2019-11-29 22:55:58 +0800

    add handy hexdump

--- src/util.cc
+++ src/util.cc
@@ -91,5 +91,36 @@ uint32_t cidr_to_mask(int prefix) {
   return boost::endian::endian_reverse(
       prefix == 0 ? 0 : (0xffffffff << (32 - prefix)));
 }
+
+void hexdump(const char *note, const void *data, size_t size) {
+  printf(">> %s\n", note);
+  char ascii[17];
+  size_t i, j;
+  ascii[16] = '\0';
+  for (i = 0; i < size; ++i) {
+    printf("%02X ", ((unsigned char *)data)[i]);
+    if (((unsigned char *)data)[i] >= ' ' &&
+        ((unsigned char *)data)[i] <= '~') {
+      ascii[i % 16] = ((unsigned char *)data)[i];
+    } else {
+      ascii[i % 16] = '.';
+    }
+    if ((i + 1) % 8 == 0 || i + 1 == size) {
+      printf(" ");
+      if ((i + 1) % 16 == 0) {
+        printf("|  %s \n", ascii);
+      } else if (i + 1 == size) {
+        ascii[(i + 1) % 16] = '\0';
+        if ((i + 1) % 16 <= 8) {
+          printf(" ");
+        }
+        for (j = (i + 1) % 16; j < 16; ++j) {
+          printf("   ");
+        }
+        printf("|  %s \n", ascii);
+      }
+    }
+  }
+}
 } // namespace util
 } // namespace khtcp
